871364840e64876a3c152b0f8a7f76eebfa180b0
Yauhen <coocooroozza@gmail.com>
2022-12-14 17:48:31 +0300

    added some more setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,13 @@ export default function App({ Component, pageProps }: AppProps): JSX.Element {
       <Head>
         <title>Next training project</title>
         <link rel="icon" href="/favicon.ico" />
+
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,300;0,500;0,600;0,700;1,400&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <Component {...pageProps} />
     </>
